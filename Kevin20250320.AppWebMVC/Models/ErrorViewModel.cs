c1a2f6a2767cedfc4e80cf4590d765e0fa0df377
Oscar Eliseo Torres Vasquez <oscar2004torres@outlook.es>
2025-03-20 07:32:29 -0600

    Agregar archivos de proyecto.

---
+++ Kevin20250320.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kevin20250320.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
